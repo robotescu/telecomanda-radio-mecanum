9ec7bae4acc2991382447c8c88b0718fc3c42827
robotescu <80849623+robotescu@users.noreply.github.com>
2022-01-15 18:02:46 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("aluneca stanga")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("aluneca dreapta")
+})
+radio.setGroup(5)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        radio.sendString("stanga")
+    } else if (input.isGesture(Gesture.TiltRight)) {
+        radio.sendString("dreapta")
+    } else if (input.isGesture(Gesture.LogoDown)) {
+        radio.sendString("fata")
+    } else if (input.isGesture(Gesture.LogoUp)) {
+        radio.sendString("spate")
+    }
+})
